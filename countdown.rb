61ba7617f34a82129b214e25b0c5de024dd88a0a
Michelle Blevins <mjblevins7@gmail.com>
2018-03-18 20:52:27 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(time)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown(time)
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
